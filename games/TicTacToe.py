524e145fd88d67b846a3fb847d91cb6b4a0d04d7
AlSaeed <w.alsaeed@hotmail.com>
2017-12-16 19:22:45 +0300

    Modified Game Logic of Tic-Tac-Toe
    1) Corrected the state representation to [Row][Col][Channel] instead of
    [Channel][Row][Col].
    
    2) Added stringify function to TicTacToe which converts a state tensor
    into human friendly representation.

--- games/TicTacToe.py
+++ games/TicTacToe.py
@@ -14,26 +14,45 @@ class TicTacToe(object):
         return np.zeros((3,3,3),dtype=np.int32)
 
     def validMoves(self,state):
-        return 1-np.max(state[:2],axis=0)
+        return 1-np.max(state[:,:,:2],axis=2)
 
     def nextState(self,state,action):
-        p = state[2][0][0]
+        p = state[0][0][2]
         rst=np.copy(state)
-        rst[p][action[0]][action[1]]=1
-        rst[2]=1-p
+        rst[action[0]][action[1]][p]=1
+        rst[:,:,2]=1-p
         return rst
 
     def score(self,state):
         for p in range(2):
             for a in range(2):
-                if np.max(np.sum(state[p],a))==3:
+                if np.max(np.sum(state[:,:,p],a))==3:
                     return 1-2*p
-            if state[p][1][1]==1 and ((state[p][0][0]==1  and state[p][2][2]==1)
-             or (state[p][0][2]==1 and state[p][2][0]==1)):
+            if state[1][1][p]==1 and ((state[0][0][p]==1  and state[2][2][p]==1)
+             or (state[0][2][p]==1 and state[2][0][p]==1)):
                 return 1-2*p
-        if np.sum(state[:2])==9:
+        if np.sum(state[:,:,:2])==9:
             return 0
         return None
     
     def player(self,state):
-        return state[2][0][0]
+        return state[0][0][2]
+
+    def stringify(self,state):
+        rst = ''
+        s = self.score(state)
+        if s:
+            if s==0:
+                rst+= 'Draw!\n'
+            elif s==1:
+                rst+= 'X Won!\n'
+            else:
+                rst+= 'O Won!\n'
+        else:
+            rst+= "Player: "+('X\n' if self.player(state)==0 else 'O\n')
+        for i in range(3):
+            A=[ 'X' if s[0]==1 else 'O' if s[1]==1 else ' ' for s in state[i] ]
+            rst+= A[0]+'|'+A[1]+'|'+A[2]+'\n'
+            if i<2:
+                rst+= '-----\n'
+        return rst
